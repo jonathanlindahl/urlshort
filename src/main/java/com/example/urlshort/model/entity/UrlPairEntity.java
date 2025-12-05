c513158ce47a8225ce429be1d480cd975f1093cd
Jonathan <jonathanlindahl3@gmail.com>
2025-12-05 14:59:01 +0100

    first draft entity

--- src/main/java/com/example/urlshort/model/entity/UrlPairEntity.java
+++ src/main/java/com/example/urlshort/model/entity/UrlPairEntity.java
@@ -1,4 +1,31 @@
 package com.example.urlshort.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class UrlPairEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private final String shortUrl;
+    private final String url;
+
+    public UrlPairEntity(String shortUrl, String url) {
+        this.shortUrl = shortUrl;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
